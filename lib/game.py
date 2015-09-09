ba3fb052076eb3d40dbbe9db7121ff03428947db
Daryl Watson <darylwtsn@gmail.com>
2015-09-09 22:12:09 +0100

    Refactored game further

--- lib/game.py
+++ lib/game.py
@@ -1,8 +1,10 @@
+import re
+
 class Game(object):
 
   def __init__(self):
-    self.board = self.create_board()
     self.letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    self.board = self.create_board()
 
   def declare_winner(self, player1, player2):
     if self.check_all_ships_sunk(player1) == True:
@@ -16,21 +18,15 @@ class Game(object):
     return True
 
   def create_board(self):
-    board = []
-    for i in range(0, 10): board.append([])
-    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    board = [[] for _ in xrange(10)]
     for i in range(1, 11):
-      for j in range(1, 11):
-        square = letters[i - 1] + str(j)
-        board[i - 1].append('~')
+      for j in range(1, 11): board[i - 1].append('~')
     return board
 
   def display_board(self, player, which='ME'):
     board = self.board
-    if which == 'ME':
-      board = self.show_ships(player, board)
-    board = self.show_shots(player, board)
-    return board
+    if which == 'ME': board = self.show_ships(player, board)
+    return self.show_shots(player, board)
 
   def show_shots(self, player, board):
     for square in player.received_shots.keys():
@@ -46,17 +42,8 @@ class Game(object):
     return board
 
   def convert(self, square):
-    result = self.split_square(square)
-    coords = [self.letters.index(result[0])]
-    coords.append(int(result[1]) - 1)
-    return coords
-
-  def split_square(self, square):
-    square = list(square)
-    if len(square) == 3:
-      square[1] = square[1] + square[2]
-      square.pop()
-    return square
+    square = re.split('(\d+)', square)
+    return [self.letters.index(square[0]), (int(square[1]) - 1)]
 
   def check_square_status(self, square, player):
     return player.received_shots.get(square) or '~'
--- tests/game_tests.py
+++ tests/game_tests.py
@@ -18,12 +18,7 @@ class TestGame(unittest.TestCase):
     self.assertEqual(len(board), 10)
 
   def test_displays_players_opponent_board(self):
-    self.player1.place_ship(Ship('Destroyer'), 'J1')
-    # self.player2.shoot(self.player1, 'J1')
-    self.player2.shoot(self.player1, 'J9')
     board = self.game.display_board(self.player1, 'ME')
-    for row in board:
-      print row
     self.assertEqual(len(board), 10)
 
   def test_says_game_over(self):
